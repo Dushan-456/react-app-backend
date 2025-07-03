665ceee386be8e2ae3232df8c4bf9deb1e19312f
Navodya Dushan <168759740+Dushan-456@users.noreply.github.com>
2025-07-03 23:03:18 +0530

    Create UserModel.js

---
+++ Model/UserModel.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    
+        name:{
+            type:String,  // data Type
+            required:true, // Validate
+        },  
+        gmail:{
+            type:String,  // data Type
+            required:true, // Validate
+        },  
+        age:{
+            type:Number,  // data Type
+            required:true, // Validate
+        },  
+        address:{
+            type:String,  // data Type
+            required:true, // Validate
+        }
+
+});
+
+module.exports = mongoose.model(
+    "UserModel",    //file name
+    userSchema      //Function name
+)
