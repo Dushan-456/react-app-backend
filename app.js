069b09629a041cbf4bbcaec01b1e60be9f6e2a21
Navodya Dushan <168759740+Dushan-456@users.noreply.github.com>
2025-07-03 17:48:06 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log("Dushannoon");
+// console.log("Dushannoon");
+//pass = 9YNvLzsgDODwzRCQ
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+// Middleware
+app.use("/",(req,res, next) => {
+        res.send("Its Working");
+})
+
+
+mongoose.connect("mongodb+srv://admin:9YNvLzsgDODwzRCQ@cluster0.4l8hnzf.mongodb.net/")
+.then(()=> console.log("Connected to Mongo DB"))
+.then(()=>{
+    app.listen(5000);
+})
+.catch((err)=>console.log((err)));
